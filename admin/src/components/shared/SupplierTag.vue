70aa6cdf20ea770ae1f212c63a0649e3e088fe1d
Sergio Kaznowiecki <skaznowiecki@gmail.com>
2023-05-25 16:53:22 -0300

    fix tag color

--- admin/src/components/shared/SupplierTag.vue
+++ admin/src/components/shared/SupplierTag.vue
@@ -2,14 +2,14 @@
   <div>
     <v-row justify="center" align="center" v-if="tags.length > 0">
       <v-col cols="auto" v-for="{ color, name } in tags">
-        <v-btn density="compact" :color="getColor(color)" variant="outlined" :disabled="true">{{
+        <v-btn class="text-white" size="small" :color="getColor(color)" :disabled="true" flat>{{
           name
         }}</v-btn>
       </v-col>
     </v-row>
     <v-row justify="center" align="center" v-else>
       <v-col cols="auto">
-        <v-btn density="compact" variant="outlined" :disabled="true">Sin Tags</v-btn>
+        <v-btn density="compact" :disabled="true">Sin Tags</v-btn>
       </v-col>
     </v-row>
   </div>
